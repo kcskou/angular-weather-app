056d319e544715e16a6870750e0965b351457ce6
Kelvin Kou <k.kou@alumni.ubc.ca>
2016-01-17 09:29:41 -0800

    fixed issue with mixed http/https protocol by using https for call to weather web service

--- services.js
+++ services.js
@@ -7,7 +7,7 @@ app.service('cityService', function () {
 app.service('weatherService', ['$resource', function($resource) {
     
     this.getWeather = function(city, days) {
-        var weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=d4c6ec0990cf92e95d45ee8b91b317fb",
+        var weatherAPI = $resource("https://api.openweathermap.org/data/2.5/forecast/daily?APPID=d4c6ec0990cf92e95d45ee8b91b317fb",
                                    { callback: "JSON_CALLBACK" },
                                    { get: { method: "JSONP" }});
   
